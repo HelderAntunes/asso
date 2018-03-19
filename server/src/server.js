693d56b474997e6436543819b9993744d45447bb
inesfproenca <inesfproenca@gmail.com>
2018-03-19 10:28:20 +0000

    Afinal trabalhei parte 2

--- server/src/server.js
+++ server/src/server.js
@@ -8,7 +8,23 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   console.log("Hello world");
-  res.send('Hello World\n');
+  var str = 'Hello World\n';
+
+  var client = require('http-rabbitmq-manager').client({
+      host : 'rabbitmq',
+  	  port : 15672,
+      timeout : 25000,
+      user : 'guest',
+      password : 'guest'
+  });
+
+  client.overview(function  (err, response) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(response);
+        }
+    });
 });
 
 app.listen(PORT, HOST);
